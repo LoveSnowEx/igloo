56330c10e93612b8e18d459c6600632314569ecd
LoveSnowEx <lsktw22@gmail.com>
2026-04-14 23:41:55 +0800

    Add initial structure with empty modules and static files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from granian import Granian
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
