819c3d91f207abb57a48fe5e5dcbe78b0c73f817
LoveSnowEx <lsktw22@gmail.com>
2026-05-28 00:07:28 +0800

    Add .gitignore and flake.nix for project configuration

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+    nixpkgs-python.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    trusted-public-keys = [
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+    extra-substituters = [
+      "https://devenv.cachix.org"
+      "https://cache.nixos.org"
+    ];
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      devenv,
+      systems,
+      ...
+    }@inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                # https://devenv.sh/reference/options/
+                packages = [ ];
+
+                languages = {
+                  python = {
+                    enable = true;
+                    version = "3.13";
+                    uv = {
+                      enable = true;
+                      sync = {
+                        enable = true;
+                        allExtras = true;
+                        allGroups = true;
+                        allPackages = true;
+                      };
+                    };
+                  };
+                };
+              }
+            ];
+          };
+        }
+      );
+    };
+}
